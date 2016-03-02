684dfc40c8a663aa7fe9476560736098591382f2
Jocke <jocke@niden.se>
2016-03-02 10:01:33 +0100

    Challenge 9, tabbed application

---
+++ Challenge 9: Tabbed application/Challenge 9: Tabbed application/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Challenge 9: Tabbed application
+//
+//  Created by Joacim Nidén on 02/03/16.
+//  Copyright © 2016 Joacim Nidén. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Challenge 9: Tabbed application/Challenge 9: Tabbed application/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  Challenge 9: Tabbed application
+//
+//  Created by Joacim Nidén on 02/03/16.
+//  Copyright © 2016 Joacim Nidén. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
