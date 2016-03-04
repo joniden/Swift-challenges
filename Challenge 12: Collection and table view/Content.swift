e1bdd7b76423ea888e101568bc45b7dd3cefce10
Jocke <jocke@niden.se>
2016-03-04 22:16:33 +0100

    Challenge 12: Segment viewcontrollers

---
+++ Challenge 12: Collection and table view/Content.swift
@@ -0,0 +1,9 @@
+//
+//  Content.swift
+//  Challenge 12: Collection and table view
+//
+//  Created by Joacim Nidén on 04/03/16.
+//  Copyright © 2016 Joacim Nidén. All rights reserved.
+//
+
+import Foundation
