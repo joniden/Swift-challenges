eda04d94f78c2c8d00cc75c595591d21229f425e
Jocke <jocke@niden.se>
2016-03-09 22:04:42 +0100

    Challenge 16

---
+++ Challenge 16: Pull to refresh/Challenge 16: Pull to refresh/TableViewController.swift
@@ -0,0 +1,127 @@
+//
+//  TableViewController.swift
+//  Challenge 16: Pull to refresh
+//
+//  Created by Joacim Nidén on 09/03/16.
+//  Copyright © 2016 Joacim Nidén. All rights reserved.
+//
+
+import UIKit
+
+struct Band {
+    let name: String
+}
+
+
+class TableViewController: UITableViewController {
+    
+    //Content of my list
+    var bands = [
+        Band(name: "Blink 182"),
+        Band(name: "Bring Me The Horizon"),
+        Band(name: "Deez Nuts"),
+        Band(name: "Less Than Jake"),
+        Band(name: "Architects"),
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.refreshControl?.addTarget(self, action: "handleRefresh:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return bands.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = bands[indexPath.row].name
+        
+        return cell
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    func handleRefresh(refreshControl: UIRefreshControl) {
+        // Do some reloading of data and update the table view's data source
+        
+        // Add this
+        let band = Band(name: "Slipknot")
+        bands.append(band)
+        
+        bands.sortInPlace() { $0.name < $1.name }
+        
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
+}
